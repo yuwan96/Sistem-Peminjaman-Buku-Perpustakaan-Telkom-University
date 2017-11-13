e794c467f89d45f6b03a074e09ad8128a9e1bb6d
Ndaru Akbar Prakoso <ndaruaprakoso@gmail.com>
2017-11-13 12:32:42 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -1,3 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ndaru
+ */
+
 public class Buku {
     private String kode,judul,pengarang,penerbit;
     private int jumlah;
