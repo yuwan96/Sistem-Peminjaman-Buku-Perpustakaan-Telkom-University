9a21a4d8c7c8e78f9f24b09382ddd0139f50d38b
Ndaru Akbar Prakoso <ndaruaprakoso@gmail.com>
2017-12-18 01:01:09 +0700

    Update Anggota.java

--- Cucumber_Sistem_Peminjaman_Buku_Perpus_TelU/src/main/java/id/impal/pinjembuku/perpustakaan/cucumber_sistem_peminjaman_buku_perpus_telu/Anggota.java
+++ Cucumber_Sistem_Peminjaman_Buku_Perpus_TelU/src/main/java/id/impal/pinjembuku/perpustakaan/cucumber_sistem_peminjaman_buku_perpus_telu/Anggota.java
@@ -5,6 +5,8 @@
  */
 package id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -12,21 +14,25 @@ import java.util.Date;
  * @author ndaru
  */
 public class Anggota {
-    private String no,nim,nama;
-    private Date massaBerlaku;
+    private String no,nim,nama; 
+    private Date masaBerlaku = new Date();
 
     public Anggota() {
+        this.no = "123";
+        this.nim = "1132124555";
+        this.nama = "Jono";
+        this.masaBerlaku = StringToDate("11/12/2017");
     }
 
-    public Anggota(String no, String nim, String nama, Date massaBerlaku) {
+    public Anggota(String no, String nim, String nama, Date masaBerlaku) {
         this.no = no;
         this.nim = nim;
         this.nama = nama;
-        this.massaBerlaku = massaBerlaku;
+        this.masaBerlaku = masaBerlaku;
     }
 
-    public void setMassaBerlaku(Date massaBerlaku) {
-        this.massaBerlaku = massaBerlaku;
+    public void setMasaBerlaku(Date masaBerlaku) {
+        this.masaBerlaku = masaBerlaku;
     }
 
     public void setNama(String nama) {
@@ -41,8 +47,8 @@ public class Anggota {
         this.no = no;
     }
 
-    public Date getMassaBerlaku() {
-        return massaBerlaku;
+    public Date getMasaBerlaku() {
+        return masaBerlaku;
     }
 
     public String getNama() {
@@ -57,4 +63,15 @@ public class Anggota {
         return no;
     }
     
+    public Date StringToDate(String s){
+    Date result = null;
+    try{
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        result  = dateFormat.parse(s);
+    }
+    catch(ParseException e){
+        e.printStackTrace();
+    }
+    return result ;
+    }
 }
