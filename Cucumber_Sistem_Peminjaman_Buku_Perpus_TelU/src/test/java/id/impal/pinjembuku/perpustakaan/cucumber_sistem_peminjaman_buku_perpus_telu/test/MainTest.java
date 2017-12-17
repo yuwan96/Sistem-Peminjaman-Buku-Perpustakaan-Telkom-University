11ed32954e45873fa0b576fb7e36104701e35c50
Ndaru Akbar Prakoso <ndaruaprakoso@gmail.com>
2017-12-17 17:02:00 +0700

    Implementasi JUnit pada method petugasLogin

--- Cucumber_Sistem_Peminjaman_Buku_Perpus_TelU/src/test/java/id/impal/pinjembuku/perpustakaan/cucumber_sistem_peminjaman_buku_perpus_telu/test/MainTest.java
+++ Cucumber_Sistem_Peminjaman_Buku_Perpus_TelU/src/test/java/id/impal/pinjembuku/perpustakaan/cucumber_sistem_peminjaman_buku_perpus_telu/test/MainTest.java
@@ -5,9 +5,22 @@
  */
 package id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.test;
 
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.Admin;
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.Anggota;
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.Buku;
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.GUI;
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.KelolaBuku;
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.Petugas;
+import id.impal.pinjembuku.perpustakaan.cucumber_sistem_peminjaman_buku_perpus_telu.tambahAnggota;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -16,5 +29,37 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class MainTest {
+    //implementasi JUnit
+    public MainTest() {
+    }
     
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testing(){
+        System.out.println("Test petugasLogin");
+        String id = "56789";
+        String nama  = "Impal";
+        String telp = "0811333444";
+        String pass = "98765";
+        Petugas instance = new Petugas();
+        boolean result = instance.petugasLogin(id, pass);
+        String expResult = "Login berhasil";
+        assertEquals(expResult, result);
+        
+    }
 }
